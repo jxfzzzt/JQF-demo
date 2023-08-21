df17ad261577c7a6aabc4dd2d6bafd6acf8ba3fe
ZhouZt <1160490625@qq.com>
2023-08-21 22:58:35 +0800

    update testAssertTrue

--- src/main/java/com/jxfzzzt/run.sh
+++ src/main/java/com/jxfzzzt/run.sh
@@ -1,1 +1,1 @@
-mvn jqf:fuzz "-Dclass=com.jxfzzzt.JQFTest" "-Dmethod=testCopy"
+mvn jqf:repro "-Dclass=com.jxfzzzt.AssertTrueTest" "-Dmethod=testAssertTrue" "-Dinput=target/fuzz-results/com.jxfzzzt.AssertTrueTest/testAssertTrue/failures/id_000000"
